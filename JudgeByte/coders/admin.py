da8dc45fb3fb972eb297754439a256f6472ebc8e
SG <44861408+FatemehGholamzadeh@users.noreply.github.com>
2021-10-03 22:09:17 +0330

    Delete JudgeByte/coders directory

--- JudgeByte/coders/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from coders.models import UserProfile, Faqs
-# Register your models here.
-
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(Faqs)
--- JudgeByte/coders/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CodersConfig(AppConfig):
-    name = 'coders'
